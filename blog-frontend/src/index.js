52052abf5c2926dab4f745e45d3ee2c6915b3eab
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-30 20:56:40 +0100

    added blog post component

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './Blog'
 // import App from './CreateAccount';
 // import App from './Login';
 // import Post from './Post'
-// import App from './Blog'
+
 
 
 ReactDOM.render(
