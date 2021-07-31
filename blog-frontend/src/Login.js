08adad35910c5cbed3c15b0959ab37d6bc8ee67d
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-31 01:42:57 +0100

    added API to login in into the user application

--- blog-frontend/src/Login.js
+++ blog-frontend/src/Login.js
@@ -1,20 +1,52 @@
+import React from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App(){
+export default class LogIn extends React.Component{
+    state = {
+       user: {
+        email: '',
+        password: ''
+        }
+    }
+
+    LogIn(){
+        axios.post(`https://stark-falls-30261.herokuapp.com/apis/user/login`, this.state.user )
+        .then((res) => {
+          console.log('This is the data', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
+    handleuserEmailChanged(event ){
+        var user = this.state.user
+        user.email = event.target.value
+        this.setState({user: user})
+      }
+    
+      handleuserPaswordChanged(event){
+        var user = this.state.user
+        user.password = event.target.value
+        this.setState({user: user})
+      }
+    
+    
+    render(){
     return(
         <div className="App">
-            <h1>Login In</h1>
-            <div className="Name">
+            <h1>Log In</h1>
+            <div className="email">
                 <p>Email</p>
-                <input className="input-field" placeholder="Email"/>
+                <input className="input-field" placeholder="Email" value={this.state.user.email} onChange={this.handleuserEmailChanged.bind(this)}/>
             </div>
-            <div className="Message">
+            <div className="pasword">
                 <p>Password</p>
-                <input className="input-field" placeholder="Password"/>
+                <input className="input-field" type="password" placeholder="Password" value={this.state.user.password} onChange={this.handleuserPaswordChanged.bind(this)} />
             </div>
-            <button className="blue-button"> Submit </button>
+            <button className="black-button" onClick={this.LogIn.bind(this)}> Submit </button>
         </div>
-    )
+    )}
 }
 
-export default App
